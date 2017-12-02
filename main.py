e83eaa38b74a81492396ecb755d5671913046023
Reloin <yihengleong@gmail.com>
2017-12-02 11:42:36 +0800

    comments

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 mmc = "Hanji"
 fmc = "Yandra"
 
+# introduction
 print("Welcome hero, to the world of Imagnus. \n I am Talia, the goddes of this world")
 filler = input("What's your name? \n")
 print("So your name is " + mmc)
 validgender = ['yes', 'no', 'y', 'n']
 gender = None
 
+# select gender
 print("Are you a male or a female, or you identify yourself as something else?")
 
+# if it's not yes or no then loop
 while gender not in validgender:
     gender = str.lower(input("Please answer yes or no \n"))
 
 
 print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
-weapon = 0
-listed = ["Sword", "Bow and Arrows", "Banana"]
+weapon = ""
+listed = ["", "Sword", "Bow and Arrows", "Banana"]
 while (weapon != 1) or (weapon != 2) or (weapon != 3):
     weapon = input("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana\n")
-
+    weapon = int(weapon)
 
 print("I see you selected " + listed[weapon])
 
