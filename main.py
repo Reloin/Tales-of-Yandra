94e4d63f9347691ccf7227ed6cb831b507417873
Reloin <yihengleong@gmail.com>
2017-12-02 11:29:17 +0800

    loop

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ gender = None
 
 print("Are you a male or a female, or you identify yourself as something else?")
 
-while (gender)
-gender = str.lower(input("Please answer yes or no \n"))
-while gender in validgender:
-    print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
-    weapon = None
-    listed[3] = {"Sword", "Bow and Arrows", "Banana"}
-    while (weapon is not 1) or (weapon is not 2) or (weapon is not 3):
-        print("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana")
-        break
+while gender not in validgender:
+    gender = str.lower(input("Please answer yes or no \n"))
 
-else:
-    print("Please answer yes or no .\n")
+
+print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
+weapon = None
+listed[3] = {"Sword", "Bow and Arrows", "Banana"}
+while (weapon is not 1) or (weapon is not 2) or (weapon is not 3):
+    print("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana")
+    break
+
 weapon = weapon - 1
 print("I see you selected " + listed[weapon])
