09f936d63627df9826f7c158f28b120a24ebf8f6
Reloin <yihengleong@gmail.com>
2017-12-02 11:01:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Hanji"
+
+print("Welcome hero, to the world of Imagnus. \n I am Talia, the goddes of this world")
+filler = input("What's your name? \n")
+print("So your name is " + name)
+
+print("Are you a male or a female, or you identify yourself as something else?")
+gender = None
+while (gender not "Yes") or (gender not "No") or (gender not "yes") or (gender not "no") or (gender not "Y") or (gender not "N") or (gender not "y") or (gender not "n"):
+    gender = input("Please answer yes or no.\n")
+
+print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
+weapon = None
+while (weapon is not 1) or (weapon is not 2) or (weapon is not 3):
+    print("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana")
