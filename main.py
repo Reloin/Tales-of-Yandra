5f6ae1f9b380dcb93094a2e4ab0cd5ca22a14a52
Reloin <yihengleong@gmail.com>
2017-12-02 11:21:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-name = "Hanji"
+mmc = "Hanji"
+fmc = "Yandra"
 
 print("Welcome hero, to the world of Imagnus. \n I am Talia, the goddes of this world")
 filler = input("What's your name? \n")
-print("So your name is " + name)
+print("So your name is " + mmc)
 validgender = ['yes', 'no', 'y', 'n']
 gender = None
 
-while True:
-    gender = str.lower(input("Are you a male or a female, or you identify yourself as something else?"))
-    if gender in validgender:
-        print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
-        weapon = None
-        while (weapon is not 1) or (weapon is not 2) or (weapon is not 3):
-            print("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana")
-            break
-        
-    else:
-        print("Please answer yes or no .\n")
-    
+print("Are you a male or a female, or you identify yourself as something else?")
+gender = str.lower(input("Please answer yes or no \n"))
+while gender in validgender:
+    print("Let's begin your journey. First you'll need a weapon, what weapon would you choose?")
+    weapon = None
+    while (weapon is not 1) or (weapon is not 2) or (weapon is not 3):
+        print("Please select with the number \n1.Sword\n2.Bow and Arrows\n3.Banana")
+        break
 
-
+else:
+    print("Please answer yes or no .\n")
